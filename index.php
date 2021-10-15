fdb075af0074a58dff25112301d92264ed5f6453
dmhlupin <45971646+dmhlupin@users.noreply.github.com>
2021-10-15 21:52:46 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+echo 'Задание 1: <br><br>';
+
+include 'task1.php';
+
+echo '<br><br> Задание 2: <br><br>';
+include 'task2.php';
+
+echo '<br><br> Задание 3: <br><br>';
+include 'task3.php';
